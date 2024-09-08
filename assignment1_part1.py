dfdcd2e86d9a3ce006bac75f6778f16eacbbdbbd
Your Name <yasmin.abdallah72@spsmail.cuny.edu>
2024-09-08 13:50:46 -0400

    changes made to respsoitory.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,21 +1,28 @@
 
-def list_divide(numbers, divide):
-    return sum(1 for number in numbers if number % divide ==0
+def list_divide(numbers, divide=2):
+    count = 0
+    for number in numbers:
+        if number % divide == 0:
+            count += 1
+    return count
 
-class ListDivideException(Exception):
-    pass
+class ListDivideException(Exception): 
+    pass 
 
-def test_list_divide():
+def test_list_divide(): 
     try:
-        assert listDivide([1,2,3,4,5]) == 2
-        assert listDivide([2,4,6,8,10]) == 5
-        assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-        assert listDivide([]) == 0
-        assert listDivide([1,2,3,4,5], 1) == 5
-    except AssertionError as e:
-        raise ListDivideExeption(f"Test failed: {e}")
-        
-if __name__ == "__main__":
+        assert list_divide([1,2,3,4,5]) == 2 
+        assert list_divide([2,4,6,8,10]) == 5 
+        assert list_divide([30, 54, 63,98, 100], divide=10) == 2 
+        assert list_divide([]) == 0  
+        assert list_divide([1,2,3,4,5], 1) == 5
+    except AssertionError: 
+        raise ListDivideException()
 
+if __name__ == "__main__":
+ try:
     test_list_divide()
-    print("All tests passed,")
+    print("All tests finally passed!!!")
+ except ListDivideException:
+     print("A test failed.")
+     
